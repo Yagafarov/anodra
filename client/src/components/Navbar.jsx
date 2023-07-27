b96f1fc7ba232554331d9a5adca9c2e438d214a0
yagafarov <dinmuhammadyagafarov@gmail.com>
2023-07-27 10:29:05 +0500

    get navbar mobile

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,44 +6,58 @@ import { faHome, faUser, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import Logo from '../img/logo.png';
 
 const Navbar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
-      <div className="container d-flex justify-content-between align-items-center">
-        {/* Logo on the left */}
-        <Link className="navbar-brand" to={'/'}>
+    return (
+    <nav className="navbar navbar-expand-md bg-body-tertiary fixed-top">
+      <div className="container">
+        {/* Logo */}
+        <Link className="navbar-brand" to="/">
           <img src={Logo} width="100" className="d-inline-block align-text-top img-flex" alt="Anodra" />
         </Link>
 
-        {/* Menu items centered */}
-        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
-          <ul className="navbar-nav">
+        {/* Hamburger menu */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Menu items */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav mx-auto">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to={'/'}>
+              <Link className="nav-link active" to="/">
                 Maqolalar
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to={'/write'}>
+              <Link className="nav-link" to="/write">
                 Yozish
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={'/about'}>
+              <Link className="nav-link" to="/about">
                 Biz haqimizda
               </Link>
             </li>
           </ul>
-        </div>
-
-        {/* Sign Up and Sign In buttons on the right */}
-        <div className='d-flex'>
-          <Link className='btn btn-outline-primary mx-3' to={'/login'}>
-          <FontAwesomeIcon icon={faUser} />
+                  {/* Sign Up and Sign In buttons */}
+        <div className="d-flex">
+          <Link className="btn btn-outline-primary" style={{marginRight:"20px"}}  to="/login">
+            <FontAwesomeIcon icon={faUser} />
           </Link>
-          <Link className='btn btn-primary text-light' to={'/register'}>
+          <Link className="btn btn-primary text-light" to="/register">
             <FontAwesomeIcon icon={faUserPlus} />
           </Link>
         </div>
+        </div>
+
+
       </div>
     </nav>
   );
